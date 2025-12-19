4b774dd0564db047a52dc6eaf78faed23d940bad
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-12-19 10:10:52 +0100

    hwpe_stream_delay_assign: make delay cycles parametric

--- rtl/basic/hwpe_stream_delay_assign.sv
+++ rtl/basic/hwpe_stream_delay_assign.sv
@@ -17,8 +17,8 @@ import hwpe_stream_package::*;
 
 module hwpe_stream_delay_assign #(
     parameter int unsigned DATA_WIDTH = 1,
-    parameter bit          DELAY_REQ = 1,
-    parameter bit          DELAY_RESP
+    parameter int unsigned DELAY_REQ  = 1,
+    parameter int unsigned DELAY_RESP = 1
 )
 (
   input  logic clk_i,
@@ -29,6 +29,8 @@ module hwpe_stream_delay_assign #(
   hwpe_stream_intf_stream.source pop_o
 );
 
+localparam int unsigned MAX_DELAY = DELAY_REQ > DELAY_RESP ? DELAY_REQ : DELAY_RESP;
+
 hwpe_stream_intf_stream #(
     .DATA_WIDTH ( DATA_WIDTH )
 `ifndef SYNTHESIS
@@ -36,41 +38,63 @@ hwpe_stream_intf_stream #(
     .BYPASS_VCR_ASSERT( 1'b1  ),
     .BYPASS_VDR_ASSERT( 1'b1  )
 `endif
-) delayed (
+) delayed [MAX_DELAY:0] (
     .clk ( clk_i )
 );
 
-if (DELAY_REQ) begin : gen_delay_req
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
-            delayed.valid <= '0;
-            delayed.data  <= '0;
-            delayed.strb  <= '0;
-        end else begin
-            delayed.valid <= push_i.valid;
-            delayed.data  <= push_i.data;
-            delayed.strb  <= push_i.strb;
+// Request
+logic delay_req;
+
+assign delay_req = (DELAY_REQ > 0) && delay_i;
+
+assign delayed[0].valid = push_i.valid;
+assign delayed[0].data  = push_i.data;
+assign delayed[0].strb  = push_i.strb;
+
+for (genvar ii = 1; ii <= MAX_DELAY; ii++) begin : gen_req
+    if (ii > DELAY_REQ) begin : gen_tieoffs
+        assign delayed[ii].valid = '0;
+        assign delayed[ii].data  = '0;
+        assign delayed[ii].strb  = '0;
+    end else begin : gen_delay
+        always_ff @(posedge clk_i or negedge rst_ni) begin
+            if (!rst_ni) begin
+                delayed[ii].valid <= '0;
+                delayed[ii].data  <= '0;
+                delayed[ii].strb  <= '0;
+            end else begin
+                delayed[ii].valid <= delayed[ii-1].valid;
+                delayed[ii].data  <= delayed[ii-1].data;
+                delayed[ii].strb  <= delayed[ii-1].strb;
+            end
         end
     end
-    assign pop_o.valid = delay_i ? delayed.valid : push_i.valid;
-    assign pop_o.data  = delay_i ? delayed.data  : push_i.data;
-    assign pop_o.strb  = delay_i ? delayed.strb  : push_i.strb;
-end else begin : gen_null_delay_req
-    assign pop_o.valid = push_i.valid;
-    assign pop_o.data  = push_i.data;
-    assign pop_o.strb  = push_i.strb;
 end
 
-if (DELAY_RESP) begin : gen_delay_resp
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
-            delayed.ready <= '0;
-        end else begin
-            delayed.ready <= pop_o.ready;
+assign pop_o.valid = delay_req ? delayed[DELAY_REQ].valid : push_i.valid;
+assign pop_o.data  = delay_req ? delayed[DELAY_REQ].data  : push_i.data;
+assign pop_o.strb  = delay_req ? delayed[DELAY_REQ].strb  : push_i.strb;
+
+// Response
+logic delay_resp;
+
+assign delay_resp = (DELAY_RESP > 0) && delay_i;
+
+assign delayed[0].ready = pop_o.ready;
+
+for (genvar ii = 1; ii <= MAX_DELAY; ii++) begin : gen_resp
+    if (ii > DELAY_RESP) begin : gen_tieoffs
+        assign delayed[ii].ready = '0;
+    end else begin : gen_delay
+        always_ff @(posedge clk_i or negedge rst_ni) begin
+            if (!rst_ni) begin
+                delayed[ii].ready <= '0;
+            end else begin
+                delayed[ii].ready <= delayed[ii-1].ready;
+            end
         end
     end
-    assign push_i.ready = delay_i ? delayed.ready : pop_o.ready;
-end else begin : gen_null_delay_resp
-    assign push_i.ready = pop_o.ready;
 end
+
+assign push_i.ready = delay_resp ? delayed[DELAY_RESP].ready : pop_o.ready;
 endmodule
